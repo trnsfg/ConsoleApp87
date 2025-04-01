7d936521ad91f563f4b1700ed5b74d150d431408
dasha <dariya.kuznetsova001.com>
2025-04-01 13:32:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        int port = 12345;
+        TcpListener listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+        Console.WriteLine($"Сервер запущен. Ожидание подключений на порту {port}...");
+
+        while (true)
+        {
+            using (TcpClient client = listener.AcceptTcpClient())
+            using (NetworkStream stream = client.GetStream())
+            {
+                byte[] buffer = new byte[256];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                string response = request.ToLower() switch
+                {
+                    "date" => DateTime.Now.ToShortDateString(),
+                    "time" => DateTime.Now.ToLongTimeString(),
+                    _ => "Неверный запрос"
+                };
+
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                stream.Write(responseBytes, 0, responseBytes.Length);
+
+                Console.WriteLine($"Запрос: {request}, Ответ: {response}");
+            }
+        }
+    }
+}
